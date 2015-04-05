c7115549824658f3138f6d4da0807636d9098054
Salem Ouerdani <salem@Salems-MacBook-Pro.local>
2015-04-05 15:00:38 +0100

    added demo page & related controller extending foundation DefaultController

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -11,10 +11,20 @@
     'foundation.dynamicRouting.animations'
   ])
     .config(config)
+    .controller('ItemCtrl', ItemCtrl)
     .run(run)
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
+  ItemCtrl.$inject = ['$scope', '$http', '$filter', '$stateParams', '$state', '$controller'];
+
+  function ItemCtrl($scope, $http, $filter, $stateParams, $state, $controller) {
+
+      angular.extend(this, $controller('DefaultController', {$scope: $scope, $filter: $filter, $http: $http, $stateParams: $stateParams, $state: $state}));
+      // what you want now...
+      $scope.test="hello";
+    
+  }
 
   function config($urlProvider, $locationProvider) {
     $urlProvider.otherwise('/');
