f8cbd00389c9bd3950e2e344b2dea7bf50bf66a5
Salem Ouerdani <tunecino@gmail.com>
2015-07-10 13:02:00 +0100

    angular - added edit page wrapped inside a modal + RestItemQuery to controller

--- client/assets/js/item/controllers.js
+++ client/assets/js/item/controllers.js
@@ -3,26 +3,37 @@
   var controllers;
 
   //ContactCtrl.$inject = ['$scope', 'MailContactQuery', 'FoundationApi'];
-  ItemCtrl.$inject = ['$scope', '$http', '$filter', '$stateParams', '$state', '$controller'];
-  function ItemCtrl($scope, $http, $filter, $stateParams, $state, $controller) {
+  ItemCtrl.$inject = ['$scope', '$http', '$filter', '$stateParams', '$state', '$controller','RestItemQuery'];
+  function ItemCtrl($scope, $http, $filter, $stateParams, $state, $controller, RestItemQuery) {
 
       //angular.extend(this, $controller('DefaultController', {$scope: $scope, $filter: $filter, $http: $http, $stateParams: $stateParams, $state: $state}));
       
+      var limit = 7;
+      $scope.page = 1;
+
+      $scope.listItems = function() {
+        RestItemQuery.getAll(limit,$scope.page).then(function(items) {
+          $scope.items = items;
+          console.log($scope.items);
+          console.log($scope.items._meta);
+        });
+      };
+
       $scope.currentItem= null;
 
-      $scope.listItems = function () {
-        $scope.items = [
-          { id: 0, name: "some hard to read french book", category:"french", price: 1.75},
-          { id: 1, name: "one more hard to read french book", category:"french", price: 2.75},
-          { id: 2, name: "piano tutorials", category:"music", price: 1.00},
-          { id: 3, name: "Guarneri del Gesù violin", category:"music", price: 3900000.00}
-        ];
-        //to make dynamique later
-        $scope.categories = [
-          { id: 0, name: "french"},
-          { id: 1, name: "music"}
-        ];
-      }
+      // $scope.listItems = function () {
+      //   $scope.items = [
+      //     { id: 0, name: "some hard to read french book", category:"french", price: 1.75},
+      //     { id: 1, name: "one more hard to read french book", category:"french", price: 2.75},
+      //     { id: 2, name: "piano tutorials", category:"music", price: 1.00},
+      //     { id: 3, name: "Guarneri del Gesù violin", category:"music", price: 3900000.00}
+      //   ];
+      //   //to make dynamique later
+      //   $scope.categories = [
+      //     { id: 0, name: "french"},
+      //     { id: 1, name: "music"}
+      //   ];
+      // }
 
       $scope.deleteItem = function (item) {
         $scope.items.splice($scope.items.indexOf(item),1);
@@ -59,8 +70,20 @@
       $scope.listItems();
   }
 
+
+  PanelCtrl.$inject = ['$scope', '$rootScope', '$http', '$filter', '$stateParams', '$state', '$controller','RestItemQuery', 'FoundationApi'];
+  function PanelCtrl($scope, $rootScope, $http, $filter, $stateParams, $state, $controller, RestItemQuery, fa) {
+    $scope.test = 'fuck off';
+    $scope.make = function() {
+      $rootScope.loading = true;
+    }
+
+
+  }
+
   controllers = {
-    ItemCtrl: ItemCtrl
+    ItemCtrl: ItemCtrl,
+    PanelCtrl: PanelCtrl
   };
 
   angular.module('ItemModule').controller(controllers);
