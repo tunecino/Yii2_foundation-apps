b3289ec29f0fb3154c47d6ccc4eada84995163ad
Salem Ouerdani <tunecino@gmail.com>
2015-07-10 12:56:33 +0100

    yii - added Cors configs

--- backend/api/modules/v1/controllers/ItemController.php
+++ backend/api/modules/v1/controllers/ItemController.php
@@ -4,9 +4,25 @@ namespace app\api\modules\v1\controllers;
 
 use yii\rest\ActiveController;
 use app\models\ItemSearch;
+use yii\filters\Cors;
+use yii\helpers\ArrayHelper;
 
 class ItemController extends ActiveController
 {
+	public function behaviors()
+	{
+	    return ArrayHelper::merge([
+	        [
+	            'class' => Cors::className(),
+	            'cors' => [
+	                'Origin' => ['*'],
+	                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+	                'Access-Control-Request-Headers' => ['*'],
+	            ],
+	        ],
+	    ], parent::behaviors());
+	}
+
     public $modelClass = 'app\models\Item';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
--- backend/api/modules/v1/controllers/TagController.php
+++ backend/api/modules/v1/controllers/TagController.php
@@ -3,9 +3,25 @@
 namespace app\api\modules\v1\controllers;
 
 use yii\rest\ActiveController;
+use yii\filters\Cors;
+use yii\helpers\ArrayHelper;
 
 class TagController extends ActiveController
 {
+	public function behaviors()
+	{
+	    return ArrayHelper::merge([
+	        [
+	            'class' => Cors::className(),
+	            'cors' => [
+	                'Origin' => ['*'],
+	                'Access-Control-Request-Method' => ['GET'],
+	                'Access-Control-Request-Headers' => ['*'],
+	            ],
+	        ],
+	    ], parent::behaviors());
+	}
+
     public $modelClass = 'app\models\Tag';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
