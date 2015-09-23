bb257dcb251c2623a81e079f435b332bb7cd3b2b
Salem Ouerdani <tunecino@gmail.com>
2015-09-23 08:02:50 +0100

    added Filtering support to TagController::indexDataProvider

--- backend/api/modules/v1/controllers/TagController.php
+++ backend/api/modules/v1/controllers/TagController.php
@@ -5,6 +5,8 @@ namespace app\api\modules\v1\controllers;
 use app\rest\ActiveController;
 use yii\filters\Cors;
 use yii\helpers\ArrayHelper;
+use yii\web\BadRequestHttpException;
+use yii\web\ServerErrorHttpException;
 
 class TagController extends ActiveController
 {
@@ -23,8 +25,41 @@ class TagController extends ActiveController
 	}
 
     public $modelClass = 'app\models\Tag';
-    public $serializer = [
-        'class' => 'app\rest\Serializer',
-        'collectionEnvelope' => 'data',
-    ];
+    public $reservedParams = ['sort','q'];
+
+    public function actions() {
+
+		$actions = parent::actions();
+		$actions['index']['prepareDataProvider'] = [$this, 'indexDataProvider'];
+
+		return $actions;
+	}
+
+	public function indexDataProvider() {
+
+		$params = \Yii::$app->request->queryParams;
+
+		$model = new $this->modelClass;
+		$modelAttr = $model->attributes;
+
+		$search = [];
+
+		if (!empty($params)) {
+			foreach ($params as $key => $value) {
+				if(!is_scalar($key) or !is_scalar($value)) {
+					throw new BadRequestHttpException('Bad Request');
+				}
+				if (!in_array(strtolower($key), $this->reservedParams) 
+					&& ArrayHelper::keyExists($key, $modelAttr, false)) {
+					$search[$key] = $value;
+				}
+			}
+		}
+
+		$searchByAttr['TagSearch'] = $search;
+
+		$searchModel = new \app\models\TagSearch();    
+        return $searchModel->search($searchByAttr);     
+	}
+
 }
